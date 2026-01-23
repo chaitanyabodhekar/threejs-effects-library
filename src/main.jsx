c1282cab09e5d88a3dac7a8115a6eaa7121fb4c1
Chaitanya Bodhekar <impossiblechaitanya@gmail.com>
2026-01-23 22:33:34 +0530

    Fix transparent liquid overlay + Vercel deployment

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { mountLiquidOverlay } from './effects/LiquidEffect01'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
 )
+
+// Mount the liquid overlay after the app loads
+setTimeout(() => {
+  mountLiquidOverlay(document.body, { overlayMode: true });
+}, 100);
